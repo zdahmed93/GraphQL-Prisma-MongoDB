5f6a9e5c27e7a6d40c84d68ac9afd79d9e264002
zdahmed93 <ahmedzribiahmed@gmail.com>
2020-03-18 15:15:34 +0100

    run some queries through the package 'prisma-binding'

--- src/prisma.js
+++ src/prisma.js
@@ -4,3 +4,11 @@ const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: 'http://localhost:4466/'
 })
+
+prisma.query.users(null, '{ id name email posts {id title published} }')
+            .then(data => console.log(JSON.stringify({users: data}, null, 2)))
+            .catch(error => console.log({error}))
+
+prisma.query.comments(null, '{ id text author { id name } }')
+            .then(data => console.log(JSON.stringify({comments: data}, null, 2)))
+            .catch(error => console.log({error}))
