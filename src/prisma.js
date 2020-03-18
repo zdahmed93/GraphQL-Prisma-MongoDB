bc2b12663b0219287e92ed7d0c0d6c7e3cb61823
zdahmed93 <ahmedzribiahmed@gmail.com>
2020-03-18 16:57:04 +0100

    set up createPost and updatePost mutations and make queries immediately after the mutation through chaining promises

--- src/prisma.js
+++ src/prisma.js
@@ -5,11 +5,18 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466/'
 })
 
+// prisma.query.users(null, '{ id name email posts {id title body published} }')
+//             .then(data => console.log(JSON.stringify({users: data}, null, 2)))
+//             .catch(error => console.log({error}))
+
+// prisma.query.comments(null, '{ id text author { id name } }')
+//             .then(data => console.log(JSON.stringify({comments: data}, null, 2)))
+//             .catch(error => console.log({error}))
 
 prisma.mutation.createPost({
     data: {
-        title: "Prisma",
-        body: "Prisma is awesome",
+        title: "Prisma with NodeJS",
+        body: "Prisma is awesome :D ",
         published: true,
         author: {
             connect: {
@@ -17,6 +24,30 @@ prisma.mutation.createPost({
             }
         }
     }
-}, '{id title body published}').then(data => console.log(JSON.stringify(data, null, 4)))
-                               .catch(error => console.log({error}))
+}, '{id title body published}').then(data => {
+                console.log(JSON.stringify(data, null, 4))
+                return prisma.query.users(null, '{ id name email posts {id title body published} }')
+            })
+            .then(users => console.log(JSON.stringify({users}, null, 2)))
+            .catch(error => console.log({error}))
+            
+
+prisma.mutation.updatePost({
+    data: {
+        body: "Prisma is an excellent tool",
+        published: false
+    },
+    where: {
+        id: "5e723b6c0274390007244964"
+    }
+    
+},
+'{id body published}'
+).then(post => {
+    console.log(JSON.stringify({post}, null, 2))
+    return prisma.query.posts(null, '{id title body author {name}}')
+}).then(posts => {
+    console.log(JSON.stringify({posts}, null, 4))
+}).catch(error => console.log({error}))
+
 
